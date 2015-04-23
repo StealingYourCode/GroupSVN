32a2167ed1cb0d26e7d0ea00ac5e0d1cb687a093
louis.yang <louis.yang@502abd72-e6fb-4a6d-be20-217205291779>
2015-04-23 14:49:10 +0000

    DAOs initialized

---
+++ ChocolateStore/src/com/fdmgroup/chocolatestore/dao/ProductDAO.java
@@ -0,0 +1,32 @@
+package com.fdmgroup.chocolatestore.dao;
+
+import com.fdmgroup.chocolatestore.interfaces.IStorable;
+import com.fdmgroup.chocolatestore.interfaces.IStorage;
+
+public class ProductDAO implements IStorage<Product, Integer>{
+
+	@Override
+	public IStorable read(Integer i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStorable create(Product t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStorable update(Product t, Product a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStorable delete(Integer i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ChocolateStore/src/com/fdmgroup/chocolatestore/dao/UserDAO.java
@@ -0,0 +1,34 @@
+package com.fdmgroup.chocolatestore.dao;
+
+import com.fdmgroup.chocolatestore.interfaces.IStorable;
+import com.fdmgroup.chocolatestore.interfaces.IStorage;
+
+public class UserDAO implements IStorage<User, Integer>{
+
+	@Override
+	public IStorable read(Integer i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStorable create(User t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStorable update(User t, User a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStorable delete(Integer i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
