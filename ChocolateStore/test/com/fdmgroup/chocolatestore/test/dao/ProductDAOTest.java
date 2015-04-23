28a34ce1e696f6a349cbd60cc6bd3a06467456fb
Antonio.Pena <Antonio.Pena@502abd72-e6fb-4a6d-be20-217205291779>
2015-04-23 19:42:21 +0000

    Finished ProductDAO Test

--- ChocolateStore/test/com/fdmgroup/chocolatestore/test/dao/ProductDAOTest.java
+++ ChocolateStore/test/com/fdmgroup/chocolatestore/test/dao/ProductDAOTest.java
@@ -4,16 +4,25 @@ import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.fdmgroup.chocolatectore.entities.Product;
 import com.fdmgroup.chocolatestore.dao.ProductDAO;
+import com.fdmgroup.chocolatestore.exceptions.NullInputException;
 import com.fdmgroup.chocolatestore.exceptions.StorableNotFoundException;
 
 public class ProductDAOTest {
 	private ProductDAO dao;
-	Product product1, product2, product3, product4, product5, product6, product7, product8;
+	Product product, product1, product2, product3, product4, product5, product6, product7, product8;
 	
+	@Before
+	public void setUp() throws Exception {
+		dao=new ProductDAO();
+	}
+	
+	@Ignore
 	@Test
 	public void testPoductIsCreated() 
 	{
@@ -27,7 +36,7 @@ public class ProductDAOTest {
 		product7 = new Product("encore!",new BigDecimal(3.99), 100, "Caramel and Biscuit enrobed in milk chocolate" );
 		product8 = new Product("Klik",new BigDecimal(4.25), 100, "Fine Milk Chocolate Crispy Bars, Filled with Nougat" );
 		
-		dao=new ProductDAO();
+		
 		try {
 			assertTrue(dao.create(product1) instanceof Product);
 			assertTrue(dao.create(product2) instanceof Product);
@@ -44,6 +53,43 @@ public class ProductDAOTest {
 		}
 	}
 	
+	@Test
+	public void testProductRead()
+	{
+		Product product;
+		try {
+			product=dao.read(3);
+			assertTrue(product.getName().equals("Snickers"));
+		} catch (StorableNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NullInputException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
-
+	@Test
+	public void testProductUpdateTest()
+	{
+		Product product = new Product("Fancy", new BigDecimal(90), 100,"this is fancy");
+		Product productupdate = product;
+		productupdate.setStockAmount(50);
+		try {
+			dao.create(product);
+			dao.update(product, productupdate);
+			try {
+				assertTrue(dao.read(11).getStockAmount().equals(50));
+				dao.delete(11);
+				assertTrue(dao.read(11).equals(null));
+			} catch (NullInputException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (StorableNotFoundException e) {
+			// TODO: handle exception
+		}
+	}
+	
 }
---
+++ ChocolateStore/test/com/fdmgroup/chocolatestore/test/dao/SaleDAOTest.java
@@ -0,0 +1,19 @@
+package com.fdmgroup.chocolatestore.test.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SaleDAOTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
