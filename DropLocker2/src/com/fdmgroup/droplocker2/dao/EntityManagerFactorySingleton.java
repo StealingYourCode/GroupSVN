c200a792b17b3dc1041d106b20a818f0a6edd4f7
Antonio.Pena <Antonio.Pena@502abd72-e6fb-4a6d-be20-217205291779>
2015-04-28 15:26:51 +0000

    Exceptions have been added and working on DAO's

---
+++ DropLocker2/src/com/fdmgroup/droplocker2/dao/EntityManagerFactorySingleton.java
@@ -0,0 +1,23 @@
+package com.fdmgroup.droplocker2.dao;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerFactorySingleton {
+		
+		private static EntityManagerFactory instance = null;
+		
+		private EntityManagerFactorySingleton(){
+			
+		}
+		
+		public static EntityManagerFactory getInstance(){
+			
+			if(instance==null)
+				instance = Persistence.createEntityManagerFactory("ChocolateStore");
+			
+			return instance;
+			
+			
+		}
+}
---
+++ DropLocker2/src/com/fdmgroup/droplocker2/exceptions/StorableNotFoundException.java
@@ -0,0 +1,29 @@
+package com.fdmgroup.droplocker2.exceptions;
+
+public class StorableNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StorableNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public StorableNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StorableNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StorableNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
