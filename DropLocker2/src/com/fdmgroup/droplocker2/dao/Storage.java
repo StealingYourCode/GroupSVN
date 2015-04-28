d8ce655f5d8c5e0de1276772aae37e230bd525b2
Antonio.Pena <Antonio.Pena@502abd72-e6fb-4a6d-be20-217205291779>
2015-04-28 16:07:05 +0000

    UserDAO added

--- DropLocker2/src/com/fdmgroup/droplocker2/dao/Storage.java
+++ DropLocker2/src/com/fdmgroup/droplocker2/dao/Storage.java
@@ -9,7 +9,7 @@ public abstract class Storage <T,I>{
 	
 	EntityManagerFactory emf = EntityManagerFactorySingleton.getInstance();
 	
-	public abstract T create (I i) throws StorableNotFoundException, StorableNullException;  
+	public abstract T create (T t) throws StorableNotFoundException, StorableNullException;  
 	public abstract T read (I i) throws StorableNotFoundException;
 	public abstract T update(T t, T y) throws StorableNotFoundException;
 	public abstract void delete(I i) throws StorableNotFoundException;
---
+++ DropLocker2/src/com/fdmgroup/droplocker2/dao/UserDAO.java
@@ -0,0 +1,84 @@
+package com.fdmgroup.droplocker2.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import com.fdmgroup.droplocker2.entities.User;
+import com.fdmgroup.droplocker2.exceptions.StorableNotFoundException;
+import com.fdmgroup.droplocker2.exceptions.StorableNullException;
+
+public class UserDAO extends Storage<User, Integer> {
+
+	EntityManager em;
+
+	@Override
+	public User create(User user) throws StorableNotFoundException,
+			StorableNullException {
+		em = emf.createEntityManager();
+
+		if (user != null) {
+			em = emf.createEntityManager();
+			em.getTransaction().begin();
+			em.persist(user);
+			em.getTransaction().commit();
+			em.close();
+			return user;
+		}
+		throw new StorableNullException(
+				"Attempted to create user of Null value");
+	}
+
+	@Override
+	public User read(Integer ID) throws StorableNotFoundException {
+
+		em = emf.createEntityManager();
+		User user = em.find(User.class, ID);
+		em.close();
+		return user;
+	}
+
+	public User read(String username) throws StorableNotFoundException {
+		em = emf.createEntityManager();
+		Query query = em.createNativeQuery(
+				"select * from user where username = '" + username + "'",
+				User.class);
+		User user;
+		try {
+			user = (User) query.getSingleResult();
+			return user;
+		} catch (NoResultException e) {
+			e.printStackTrace();
+			throw new StorableNotFoundException("User not found");
+		} finally {
+			em.close();
+		}
+	}
+
+	@Override
+	public User update(User oldUser, User newUser)
+			throws StorableNotFoundException {
+		em = emf.createEntityManager();
+		if (oldUser != null && newUser != null) {
+			em.getTransaction().begin();
+			User result = em.find(User.class, oldUser.getUserId());
+			result.setPassword(newUser.getPassword());
+			result.setUsername(newUser.getUsername());
+			result.setUserPlan(newUser.getUserPlan());
+			em.close();
+			return newUser;
+		} else
+			throw new StorableNotFoundException(oldUser
+					+ " was not found in our database");
+	}
+
+	@Override
+	public void delete(Integer ID) throws StorableNotFoundException {
+		em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.remove(em.find(User.class, ID));
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
